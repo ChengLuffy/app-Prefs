bddd9c9c9326022b6685b42b15d2d8ab89637a66
成璐飞 <chengluffy@hotmail.com>
2017-02-06 01:32:31 +0800

    fixed app-Prefs.plist's errors

--- app-Prefs/AboutViewController.swift
+++ app-Prefs/AboutViewController.swift
@@ -213,7 +213,89 @@ extension AboutViewController: UITableViewDelegate, UITableViewDataSource {
                 let session = URLSession(configuration: sessionConfig)
                 let task = session.downloadTask(with: URL.init(string: (textField?.text!)!)!, completionHandler: { (url, response, error) in
                     print(url)
-                    try! FileManager.default.moveItem(at: url!, to: URL.init(string: NSTemporaryDirectory()+"app-Prefs.plist")!)
+                    if FileManager.default.fileExists(atPath: NSTemporaryDirectory()+"app-Prefs.plist") {
+                        try! FileManager.default.removeItem(atPath: NSTemporaryDirectory()+"app-Prefs.plist")
+                    }
+                    try! FileManager.default.moveItem(at: url!, to: URL.init(fileURLWithPath: NSTemporaryDirectory()+"app-Prefs.plist"))
+                    let fileUrl = URL.init(fileURLWithPath: NSTemporaryDirectory()+"app-Prefs.plist")
+                    
+                    let dict = NSDictionary(contentsOf: fileUrl) as! Dictionary<String, AnyObject>
+                    if dict.keys.contains("name") && dict.keys.contains("settings") {
+                        if (dict["name"] as! String == "app-Prefs") && (dict["settings"]?.isKind(of: NSArray.self))! {
+                            let tempArr = dict["settings"] as! NSArray
+                            var ret = true
+                            for tempDict in tempArr {
+                                if (tempDict as AnyObject).isKind(of: NSDictionary.self) {
+                                    if ((tempDict as! NSDictionary).allKeys as NSArray).contains("action") && ((tempDict as! NSDictionary).allKeys as NSArray).contains("name") && ((tempDict as! NSDictionary).allKeys as NSArray).contains("isDeleted") && ((tempDict as! NSDictionary).allKeys as NSArray).contains("sortNum") && ((tempDict as! NSDictionary).allKeys as NSArray).contains("type") {
+                                        if ((tempDict as! NSDictionary)["action"] as AnyObject).isKind(of: NSString.self) && ((tempDict as! NSDictionary)["name"] as AnyObject).isKind(of: NSString.self) && ((tempDict as! NSDictionary)["type"] as AnyObject).isKind(of: NSString.self) && ((tempDict as! NSDictionary)["sortNum"] as AnyObject).isKind(of: NSNumber.self) && (((tempDict as! NSDictionary)["isDeleted"] as? Bool == false) || ((tempDict as! NSDictionary)["isDeleted"] as? Bool == true))  {
+                                            
+                                        } else {
+                                            ret = false
+                                        }
+                                    } else {
+                                        ret = false
+                                    }
+                                } else {
+                                    ret = false
+                                }
+                            }
+                            if ret != true {
+                                self.alertWrongFormat()
+                                return
+                            }
+                            let alertC = UIAlertController(title: NSLocalizedString("Warning", comment: ""), message: NSLocalizedString("importWarning", comment: ""), preferredStyle: .alert)
+                            let cancelAction = UIAlertAction(title: NSLocalizedString("Cancel", comment: ""), style: .cancel, handler: { (_) in
+                            })
+                            let sureAction = UIAlertAction(title: NSLocalizedString("Sure", comment: ""), style: .destructive, handler: { (_) in
+                                let container = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.chengluffy.app-Prefs")
+                                let realmURL = container!.appendingPathComponent("defualt.realm")
+                                Realm.Configuration.defaultConfiguration.fileURL = realmURL
+                                do {
+                                    let realm = try! Realm()
+                                    try realm.write {
+                                        realm.deleteAll()
+                                    }
+                                    try realm.write {
+                                        let arr = dict["settings"] as! Array<Dictionary<String, Any>>
+                                        for subDict in arr {
+                                            let model = Setting()
+                                            model.action = subDict["action"] as! String
+                                            model.name = subDict["name"] as! String
+                                            model.isDeleted = subDict["isDeleted"] as! Bool
+                                            model.sortNum = subDict["sortNum"] as! NSNumber
+                                            model.type = subDict["type"] as! String
+                                            print(model.name)
+                                            realm.add(model)
+                                        }
+                                    }
+                                } catch {
+                                    print(error)
+                                }
+                                
+                                (self.navigationController?.viewControllers.first as! ViewController).refresh()
+                                try! FileManager.default.removeItem(at: fileUrl)
+                                
+                            })
+                            alertC.addAction(cancelAction)
+                            alertC.addAction(sureAction)
+                            
+                            self.present(alertC, animated: true, completion: {
+                                
+                            })
+                            
+                        } else {
+                            print("name' value isn't app-Prefs")
+                            self.alertWrongFormat()
+                            try! FileManager.default.removeItem(at: fileUrl)
+                        }
+                    } else {
+                        print("no key: name")
+                        self.alertWrongFormat()
+                        try! FileManager.default.removeItem(at: fileUrl)
+                    }
+
+
+                    
                 })
                 task.resume()
             })
@@ -225,6 +307,16 @@ extension AboutViewController: UITableViewDelegate, UITableViewDataSource {
         default: break
         }
         
+    }
+    
+    func alertWrongFormat() {
+        let alertC = UIAlertController(title: NSLocalizedString("Warning", comment: ""), message: NSLocalizedString("WrongFormat", comment: ""), preferredStyle: .alert)
+        present(alertC, animated: true, completion: {
+            DispatchQueue.main.asyncAfter(deadline: DispatchTime.now()+1.25, execute: {
+                alertC.dismiss(animated: true, completion: {
+                })
+            })
+        })
     }
     
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
