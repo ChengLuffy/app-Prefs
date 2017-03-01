4dd545f3c532bbe4a7cec06312ca30e84ef47d9d
成璐飞 <chengluffy@hotmail.com>
2017-03-01 23:40:21 +0800

    rewrite import action

--- app-Prefs/Controller/AboutViewController.swift
+++ app-Prefs/Controller/AboutViewController.swift
@@ -243,79 +243,11 @@ extension AboutViewController: UITableViewDelegate, UITableViewDataSource {
                         try! FileManager.default.moveItem(at: url!, to: URL.init(fileURLWithPath: NSTemporaryDirectory()+"app-Prefs.plist"))
                         let fileUrl = URL.init(fileURLWithPath: NSTemporaryDirectory()+"app-Prefs.plist")
                         
-                        if let dict = NSDictionary(contentsOf: fileUrl) as? Dictionary<String, AnyObject> {
-                            if dict.keys.contains("name") && dict.keys.contains("settings") {
-                                if (dict["name"] as! String == "app-Prefs") && (dict["settings"]?.isKind(of: NSArray.self))! {
-                                    let tempArr = dict["settings"] as! NSArray
-                                    var ret = true
-                                    for tempDict in tempArr {
-                                        if (tempDict as AnyObject).isKind(of: NSDictionary.self) {
-                                            if ((tempDict as! NSDictionary).allKeys as NSArray).contains("action") && ((tempDict as! NSDictionary).allKeys as NSArray).contains("name") && ((tempDict as! NSDictionary).allKeys as NSArray).contains("isDeleted") && ((tempDict as! NSDictionary).allKeys as NSArray).contains("sortNum") && ((tempDict as! NSDictionary).allKeys as NSArray).contains("type") {
-                                                if ((tempDict as! NSDictionary)["action"] as AnyObject).isKind(of: NSString.self) && ((tempDict as! NSDictionary)["name"] as AnyObject).isKind(of: NSString.self) && ((tempDict as! NSDictionary)["type"] as AnyObject).isKind(of: NSString.self) && ((tempDict as! NSDictionary)["sortNum"] as AnyObject).isKind(of: NSNumber.self) && (((tempDict as! NSDictionary)["isDeleted"] as? Bool == false) || ((tempDict as! NSDictionary)["isDeleted"] as? Bool == true))  {
-                                                    
-                                                } else {
-                                                    ret = false
-                                                }
-                                            } else {
-                                                ret = false
-                                            }
-                                        } else {
-                                            ret = false
-                                        }
-                                    }
-                                    if ret != true {
-                                        self.alertWrongFormat("WrongFormat")
-                                        return
-                                    }
-                                    let alertC = UIAlertController(title: SwitchLanguageTool.getLocalString(of: "Warning"), message: SwitchLanguageTool.getLocalString(of: "importWarning"), preferredStyle: .alert)
-                                    let cancelAction = UIAlertAction(title: SwitchLanguageTool.getLocalString(of: "Cancel"), style: .cancel, handler: { (_) in
-                                    })
-                                    let sureAction = UIAlertAction(title: SwitchLanguageTool.getLocalString(of: "Sure"), style: .destructive, handler: { (_) in
-                                        do {
-                                            let realm = try! Realm()
-                                            try realm.write {
-                                                realm.deleteAll()
-                                            }
-                                            try realm.write {
-                                                let arr = dict["settings"] as! Array<Dictionary<String, Any>>
-                                                for subDict in arr {
-                                                    let model = Setting()
-                                                    model.action = subDict["action"] as! String
-                                                    model.name = subDict["name"] as! String
-                                                    model.isDeleted = subDict["isDeleted"] as! Bool
-                                                    model.sortNum = subDict["sortNum"] as! NSNumber
-                                                    model.type = subDict["type"] as! String
-                                                    print(model.name)
-                                                    realm.add(model)
-                                                }
-                                            }
-                                        } catch {
-                                            print(error)
-                                        }
-                                        
-                                        (self.navigationController?.viewControllers.first as! ViewController).refresh()
-                                        try! FileManager.default.removeItem(at: fileUrl)
-                                        SVProgressHUD.showSuccess(withStatus: SwitchLanguageTool.getLocalString(of: "Success!"))
-                                    })
-                                    alertC.addAction(cancelAction)
-                                    alertC.addAction(sureAction)
-                                    SVProgressHUD.dismiss()
-                                    self.present(alertC, animated: true, completion: {
-                                    })
-                                    
-                                } else {
-                                    print("name' value isn't app-Prefs")
-                                    self.alertWrongFormat("WrongFormat")
-                                    try! FileManager.default.removeItem(at: fileUrl)
-                                }
-                            } else {
-                                print("no key: name")
-                                self.alertWrongFormat("WrongFormat")
-                                try! FileManager.default.removeItem(at: fileUrl)
-                            }
-                            
+                        let ret = ConfigTool.import(from: fileUrl)
+                        if ret == true {
+                            SVProgressHUD.showSuccess(withStatus: SwitchLanguageTool.getLocalString(of: "importSuccess"))
                         } else {
-                            self.alertWrongFormat("WrongFormat")
+                            SVProgressHUD.showError(withStatus: SwitchLanguageTool.getLocalString(of: "WrongFormat"))
                         }
                         
                     })
--- app-Prefs/Tool/ConfigTool.swift
+++ app-Prefs/Tool/ConfigTool.swift
@@ -49,7 +49,88 @@ class ConfigTool {
         
     }
     
-    class func `import`(from fileUrl: URL) {
+    class func `import`(from fileUrl: URL) -> Bool {
+        
+        if fileUrl.absoluteString.hasSuffix(".plist") {
+            if let dict = NSDictionary(contentsOf: fileUrl) as? Dictionary<String, AnyObject> {
+                if dict.keys.contains("name") && dict.keys.contains("settings") {
+                    if (dict["name"] as! String == "app-Prefs") && (dict["settings"]?.isKind(of: NSArray.self))! {
+                        let tempArr = dict["settings"] as! NSArray
+                        var ret = true
+                        for tempDict in tempArr {
+                            if (tempDict as AnyObject).isKind(of: NSDictionary.self) {
+                                if ((tempDict as! NSDictionary).allKeys as NSArray).contains("action") && ((tempDict as! NSDictionary).allKeys as NSArray).contains("name") && ((tempDict as! NSDictionary).allKeys as NSArray).contains("isDeleted") && ((tempDict as! NSDictionary).allKeys as NSArray).contains("sortNum") && ((tempDict as! NSDictionary).allKeys as NSArray).contains("type") {
+                                    if ((tempDict as! NSDictionary)["action"] as AnyObject).isKind(of: NSString.self) && ((tempDict as! NSDictionary)["name"] as AnyObject).isKind(of: NSString.self) && ((tempDict as! NSDictionary)["type"] as AnyObject).isKind(of: NSString.self) && ((tempDict as! NSDictionary)["sortNum"] as AnyObject).isKind(of: NSNumber.self) && (((tempDict as! NSDictionary)["isDeleted"] as? Bool == false) || ((tempDict as! NSDictionary)["isDeleted"] as? Bool == true))  {
+                                        
+                                    } else {
+                                        ret = false
+                                    }
+                                } else {
+                                    ret = false
+                                }
+                            } else {
+                                ret = false
+                            }
+                        }
+                        if ret != true {
+                            SVProgressHUD.showError(withStatus: SwitchLanguageTool.getLocalString(of: "WrongFormat"))
+                            return false
+                        }
+                        let container = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.chengluffy.app-Prefs")
+                        let realmURL = container!.appendingPathComponent("defualt.realm")
+                        Realm.Configuration.defaultConfiguration.fileURL = realmURL
+                        do {
+                            let realm = try! Realm()
+                            try realm.write {
+                                realm.deleteAll()
+                            }
+                            try realm.write {
+                                let arr = dict["settings"] as! Array<Dictionary<String, Any>>
+                                for subDict in arr {
+                                    let model = Setting()
+                                    model.action = subDict["action"] as! String
+                                    model.name = subDict["name"] as! String
+                                    model.isDeleted = subDict["isDeleted"] as! Bool
+                                    model.sortNum = subDict["sortNum"] as! NSNumber
+                                    model.type = subDict["type"] as! String
+                                    print(model.name)
+                                    realm.add(model)
+                                }
+                            }
+                        } catch {
+                            print(error)
+                            SVProgressHUD.showError(withStatus: SwitchLanguageTool.getLocalString(of: "WrongFormat"))
+                            return false
+                        }
+                        ((UIApplication.shared.delegate as! AppDelegate).window?.rootViewController as! UINavigationController).popToRootViewController(animated: true)
+                        (((UIApplication.shared.delegate as! AppDelegate).window?.rootViewController as! UINavigationController).viewControllers.first as! ViewController).refresh()
+                        try! FileManager.default.removeItem(at: fileUrl)
+                        SVProgressHUD.showSuccess(withStatus: SwitchLanguageTool.getLocalString(of: "Success!"))
+                        return true
+                    } else {
+                        print("name' value isn't app-Prefs")
+                        SVProgressHUD.showError(withStatus: SwitchLanguageTool.getLocalString(of: "WrongFormat"))
+                        try! FileManager.default.removeItem(at: fileUrl)
+                        return false
+                    }
+                } else {
+                    print("no key: name")
+                    SVProgressHUD.showError(withStatus: SwitchLanguageTool.getLocalString(of: "WrongFormat"))
+                    try! FileManager.default.removeItem(at: fileUrl)
+                    return false
+                }
+                
+            } else {
+                SVProgressHUD.showError(withStatus: SwitchLanguageTool.getLocalString(of: "WrongFormat"))
+                try! FileManager.default.removeItem(at: fileUrl)
+                return false
+            }
+        } else {
+            print("not a plist file")
+            SVProgressHUD.showError(withStatus: SwitchLanguageTool.getLocalString(of: "WrongFormat"))
+            try! FileManager.default.removeItem(at: fileUrl)
+            return false
+        }
         
     }
     
