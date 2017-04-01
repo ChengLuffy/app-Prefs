ba0904d77916318d9a545252af554d8d884243cf
成璐飞 <chengluffy@hotmail.com>
2017-04-01 13:26:01 +0800

    want to create ClipboardActionTool

---
+++ app-Prefs/Tool/ClipboardActionTool.swift
@@ -0,0 +1,18 @@
+//
+//  ClipboardActionTool.swift
+//  app-Prefs
+//
+//  Created by 成殿 on 2017/4/1.
+//  Copyright © 2017年 成璐飞. All rights reserved.
+//
+
+import Foundation
+
+class ClipboardActionTool {
+    
+    // TODO: 归类剪切板操作
+    func performAction(_ action: String) -> Bool {
+        return true;
+    }
+    
+}
